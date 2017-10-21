11b0758e248236d37744d719cf7723e5f8355d84
amir-arad <amira@wix.com>
2017-10-21 10:34:43 +0300

    add UDP listening capabilities

---
+++ tools-and-poc/udp-listen.js
@@ -0,0 +1,11 @@
+
+var server = require('dgram').createSocket('udp4');
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+server.bind(8000);
