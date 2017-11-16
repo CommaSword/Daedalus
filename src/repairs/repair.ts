a44f754e25bc642d7a1598c2a6ffb8c6c79fd2c6
Yuval Moran <yuvalg@gmail.com>
2017-11-16 03:02:29 +0200

    Completed repair module + made changes to system module

--- src/repairs/repair.ts
+++ src/repairs/repair.ts
@@ -1,16 +1,50 @@
 import {System1, System2} from "./systems";
 
 export class RepairModule {
-    public repairing: System1;
+    private _repairing: string = '';
+    private firstLevelSystems: { [systemName: string]: System1 };
+    private secondLevelSystems: { [systemName: string]: System2 };
 
-    private firstLevelSystems: System1[];
-    private secondLevelSystems: System2[];
-
-    private first2secondMap: { [system1Name: string]: System2[] } = {};
-    private second2firstMap: { [system2Name: string]: System1[] } = {};
+    //private first2secondMap: { [system1Name: string]: System2[] } = {};
+    private static readonly second2firstMap: { [system2Name: string]: string[] } = {
+        'zeroPointModule': ['reactor', 'beams'],
+        'activeCollector': ['beams', 'missile'],
+        'polaronLimiter': ['maneuvering', 'impulse', 'jumpDrive'],
+        'nanowaveShiftEnergizer': ['frontShield', 'rearShield', 'reactor'],
+        'coaxialPlasmaCapacitor': ['missile', 'maneuvering', 'frontShield'],
+        'dilithiumParticleGenerator': ['impulse', 'jumpDrive', 'rearShield'],
+    };
 
     constructor() {
-
+        for (let secondarySystem of Object.keys(RepairModule.second2firstMap)) {
+            let sys2 = new System2(secondarySystem);
+            for (let primarySystem of RepairModule.second2firstMap[secondarySystem]) {
+                if (!Object.keys(this.firstLevelSystems).indexOf(primarySystem)) {
+                    this.firstLevelSystems[primarySystem] = new System1(primarySystem);
+                }
+                this.firstLevelSystems[primarySystem].supportingSystems.push(sys2);
+                sys2.supportedSystems.push(this.firstLevelSystems[primarySystem]);
+            }
+            this.secondLevelSystems[secondarySystem] = sys2;
+        }
     }
 
+    get repairing(): string {
+        return this._repairing;
+    }
+
+    startRepairing(sysName: string) {
+        if (this._repairing) {
+            this.stopRepairing()
+        }
+        this._repairing = sysName;
+        this.firstLevelSystems[sysName].startRepairTicker();
+    }
+
+    stopRepairing() {
+        if (this._repairing) {
+            this.firstLevelSystems[this._repairing].stopRepairTicker();
+        }
+        this._repairing = '';
+    }
 }
--- src/repairs/systems.ts
+++ src/repairs/systems.ts
@@ -1,23 +1,30 @@
+import Timer = NodeJS.Timer;
+
 export class System1 {
     public name: string;
     public health: number;
     public heat_level: number;
     public coolant: number;
     public static readonly maxHealth = 1.0;
+    public static readonly absoluteMaxPower = 3.0;
+    public static readonly unsupportedMaxPower = 1.0;
+    public supportingSystems: Array<any>;
 
     private _power: number = 0;
     private maxPower: number;
     private repairRate: number;
+    private repairTicker: Timer;
     private static readonly baseRepairRate = 0.01;
-    private static readonly unsupportedMaxPower = 1.0;
-    private static readonly absoluteMaxPower = 3.0;
-    private supportingSystems: Array<any>;
+    private static readonly tickTime = 1000;
 
-    constructor(name: string, health: number, temperature: number, initPower: number,
-                coolant: number, supportingSystems: Array<any>) {
+
+
+    constructor(name: string, health: number = System1.maxHealth, heat_level: number = 0,
+                initPower: number = System1.unsupportedMaxPower, coolant: number = 0,
+                supportingSystems: Array<any> = []) {
         this.name = name;
         this.health = health;
-        this.heat_level = temperature;
+        this.heat_level = heat_level;
         this.supportingSystems = supportingSystems;
         this.setMaxPower();
         this.power = initPower;
@@ -62,14 +69,30 @@ export class System1 {
         }
         this._power = newPower;
         if (this._power > System1.unsupportedMaxPower) {
-            this.corruptSubSystems(this._power - System1.unsupportedMaxPower);
+            for (let sys of this.supportingSystems) {
+                sys.startCorruptionTicker(this._power - System1.unsupportedMaxPower);
+            }
+        } else {
+            for (let sys of this.supportingSystems) {
+                sys.stopCorruption();
+            }
         }
     }
 
-    corruptSubSystems(step: number) {
-        for (let sys of this.supportingSystems) {
-            sys.startCorruptionTicker(step);
+    private repairSystem = () => {
+        this.health += this.repairRate;
+        if (this.health >= System1.maxHealth) {
+            this.health = System1.maxHealth;
+            clearInterval(this.repairTicker);
         }
+    };
+
+    startRepairTicker() {
+        this.repairTicker = setInterval(this.repairSystem, System1.tickTime);
+    }
+
+    stopRepairTicker() {
+        clearInterval(this.repairTicker);
     }
 }
 
@@ -78,12 +101,13 @@ export class System2 {
     public name: string;
     public error: boolean;
     public isOnline: boolean;
+    public supportedSystems: System1[];
 
     private corruption: number = 0;
     private corruptionRate: number;
     private corruptionThreshold: number;
-    private corruptionTicker: number;
-    private supportedSystems: System1[];
+    private corruptionTicker: Timer;
+
 
     private static readonly tickTime = 1000;
     private static readonly baseCorruption = 0.001;
@@ -128,21 +152,22 @@ export class System2 {
         }
     }
 
-    corruptSystem(rate: number) {
-        this.corruption += rate * System2.baseCorruption;
+    private corruptSystem = () => {
+        this.corruption += this.corruptionRate * System2.baseCorruption;
         if (this.corruption >= this.corruptionThreshold) {
             if (this.corruptionTicker) {
                 clearInterval(this.corruptionTicker);
             }
             this.setError(true);
         }
-    }
+    };
 
     startCorruptionTicker(rate: number) {
         if (this.corruptionTicker) {
             clearInterval(this.corruptionTicker);
         }
-        this.corruptionTicker = setInterval(this.corruptSystem(rate), System2.tickTime);
+        this.corruptionRate = rate;
+        this.corruptionTicker = setInterval(this.corruptSystem, System2.tickTime);
     }
 
     stopCorruption() {
