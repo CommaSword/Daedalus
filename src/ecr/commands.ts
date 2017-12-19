8c3b72d0bd006718481c842f1446032e4612566a
amir-arad <greenshade@gmail.com>
2017-12-19 15:48:28 +0200

    fixes constants (tweaking) added password for loading ECR module

--- src/ecr/commands.ts
+++ src/ecr/commands.ts
@@ -1,35 +1,33 @@
-import {normInputNumber, Request, withUser} from "../session/command-utils";
+import {withUser} from "../session/command-utils";
 import {RootModuleBuilder} from "@fugazi/connector/scripts/bin/components";
 import {EcrModuleClient} from "./rpc";
 import {ESystem} from "../empty-epsilon/model";
 
 export default function init(builder: RootModuleBuilder, repair: EcrModuleClient) {
+
     builder
         .descriptor({
-            title: "ecr"
+            title: "ecr commands"
         })
-        .command("repair", {
-            title: "repair",
-            returns: "string",
-            syntax: "repair system (systemId number)"
-        })
-        .handler(withUser(async (request: Request) => {
-            const systemId = normInputNumber(request.data("systemId"));
-            if (systemId > ESystem.None && systemId < ESystem.COUNT) {
-                await repair.beginRepair(systemId);
-                return `System ${ESystem[systemId]} automatic repair routine engaged`;
-            } else {
-                throw new Error(`Invalid system ID (${ESystem.None + 1} - ${ESystem.COUNT - 1})`);
-            }
-        })).parent()
         .command('stop', {
             title: 'stop',
-            returns: 'string',
+            returns: 'ui.message',
             syntax: 'stop repair'
         })
         .handler(withUser(async () => {
             await repair.stopRepair();
             return `automatic repair routine inactive`;
-        })).parent()
-    ;
+        })).parent();
+
+    Array.from(Array(ESystem.COUNT))
+        .forEach((_, systemId) => builder
+            .command(`repair_${ESystem[systemId]}`, {
+                title: "repair",
+                returns: "ui.message",
+                syntax: `repair ${ESystem[systemId]}`
+            })
+            .handler(withUser(async () => {
+                await repair.beginRepair(systemId);
+                return `System ${ESystem[systemId]} automatic repair routine engaged`;
+            })).parent());
 }
